de30211c5de106e7a915c7b928598c7702f1c394
Amirmohammad Akbari <111335824+KingdomAmirmo@users.noreply.github.com>
2025-03-04 15:32:06 +0330

    Update 02F.js

--- 02F.js
+++ 02F.js
@@ -1,7 +1,7 @@
 let input = process.argv.slice(2);
 let operation = input[0];
-let numbers = input.slice(1)
-
+// let numbers = input.slice(1)
+let inputs = input.slice(1)
 
 if (operation === "Sum") {
     console.log(Number(numbers[0]) + Number(numbers[1]));
@@ -9,9 +9,9 @@ if (operation === "Sum") {
     console.log(Number(numbers[0]) - Number(numbers[1]));
 } else if (operation === "Print") {
     let user = {
-        name: "Amir",
-        last_name: "Akbari",
-        email: "amir@yahoo.com"
+        name: inputs[0],
+        last_name: inputs[1],
+        email: inputs[2]
     }
     for(let key in user) {
         console.log("Salam" + ' ' + user[key]);
