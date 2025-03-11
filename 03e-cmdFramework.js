d5fa0cc89f9e978a26b168746d529fea73dd97f1
Amirmohammad Akbari <111335824+KingdomAmirmo@users.noreply.github.com>
2025-03-11 16:47:16 +0330

    Create 03e-cmdFramework.js

---
+++ 03e-cmdFramework.js
@@ -0,0 +1,25 @@
+let input = process.argv.slice(3);
+let command = process.argv[2];
+let controllers = [];
+
+function start() {
+    for (let controller of controllers) {
+        if (controller.command === command) {
+            controller.function(input);
+        }
+    }
+}
+
+function use(name, func) {
+    let item = {
+        command: name,
+        function: func
+    }
+}
+
+export {
+    start,
+    use
+}
+
+
